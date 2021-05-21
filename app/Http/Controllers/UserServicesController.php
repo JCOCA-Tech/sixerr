5fb5664779c57d48bfccfdb05f03040de5f1f834
JCOCA-Tech <jcoca@pm.me>
2021-05-21 03:21:15 +0200

    created ER and reformatted the UserServicesController

--- app/Http/Controllers/UserServicesController.php
+++ app/Http/Controllers/UserServicesController.php
@@ -4,15 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\UserService;
-use DB;
+use Illuminate\Support\Str;
 
 class UserServicesController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         //$userServices = UserService::all();
@@ -20,71 +15,42 @@ class UserServicesController extends Controller
         //$userServices = DB::select('select *from userServices;');
         return view('userServices.index')->with('userServices',$userServices);
     }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         // TODO create function @UserServicesController
     }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         //TODO archive function @UserServicesController
     }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         $userService = UserService::find($id);
         return view('userServices.show')->with('userService', $userService);
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
         // TODO edit function @UserServicesController
     }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
         // TODO update function @UserServicesController
     }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         // TODO delete function @UserServicesController
+    }
+    public function search(Request $request)
+    {
+        $key = Str::lower(trim($request->get('query')));
+
+        $posts = UserService::query()
+            ->where('title', 'like', "%{$key}%")
+            ->orderBy('created_at', 'desc')
+            ->get();
+
+        return view('posts.index', [
+            'posts' => $posts
+        ]);
     }
 }
